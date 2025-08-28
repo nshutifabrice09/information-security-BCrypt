64d77e9c228ba64ad2f9b35ec3f3e3b8bb614e84
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-28 08:28:28 +0200

    changes on Hash and Compare Passwords Asynchronously

--- server.js
+++ server.js
@@ -9,9 +9,25 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-app.use('/', (req, res) =>{
-  res.send("Information Security, BCrypt Hashes");
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare('passw0rd!', hash, (err, res) => {
+    console.log(res); //true
+    });
+  });
 });
+
+
+
+// app.use('/', (req, res) =>{
+//   res.send("Information Security, BCrypt Hashes");
+// });
+
+
+
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
