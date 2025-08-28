cbcacd5e2a02471cc142ebc4b4695710f656fa63
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-28 09:19:33 +0200

    change of codes on Hash and Compare Passwords Asynchronously

--- server.js
+++ server.js
@@ -9,29 +9,19 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-  console.log(hash);
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
-  console.log(hash);
-  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-  bcrypt.compare('passw0rd!', hash, (err, res) => {
-    console.log(res); //true
-    });
-  });
+app.use('/', (req, res) =>{
+  res.send("Information Security, BCrypt Hashes");
 });
-
-
-
-// app.use('/', (req, res) =>{
-//   res.send("Information Security, BCrypt Hashes");
-// });
-
-
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 
 //END_ASYNC
 
