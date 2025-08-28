b4283586a164dbbd24b58a4792c0c431cd31d828
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-28 09:30:14 +0200

    Add a synchronous bcrypt

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/', (req, res) =>{
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     console.log(res);
@@ -27,7 +27,10 @@ const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 //START_SYNC
 
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
